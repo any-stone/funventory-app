6bc6baf24a1881211d55f4abcfa9699130da5609
Annie Kuxa <kuxa.business@gmail.com>
2023-04-24 11:40:59 -0300

    add review model

---
+++ models/review.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const reviewSchema = new Schema({
+  rating: {type: Number, min: 1, max: 5, default: 5},
+  text: {type: String, required: true},
+  game: {type: Schema.Types.ObjectId, ref: 'Game'},
+  author: {type: Schema.Types.ObjectId, ref: 'Profile'}
+}, {
+  timestamps: true
+})
+
+const Review = mongoose.model('Review', reviewSchema)
+
+export {
+  Review
+}
