f4ee07072c9a847ac4a8073ad25b6ad573354fda
Annie Kuxa <kuxa.business@gmail.com>
2023-04-24 12:52:44 -0300

    add the review is now possible only for logged in users

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,11 +1,12 @@
 import { Router } from 'express'
 import * as reviewsCtrl from '../controllers/reviews.js'
+import { isLoggedIn } from '../middleware/middleware.js'
 
 const router = Router()
 
 router.get('/:gameId', reviewsCtrl.createReview)
-router.post('/:gameId/reviews', reviewsCtrl.createReview);
+router.post('/:gameId/reviews', isLoggedIn, reviewsCtrl.createReview);
 
 export {
-  router
+  router,
 }
