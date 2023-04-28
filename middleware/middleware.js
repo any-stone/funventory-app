d723844ea793cf1dd6954b10ba119fcc0fd56008
Annie Kuxa <kuxa.business@gmail.com>
2023-04-27 21:41:52 -0300

    fix minore bugs on index page

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -1,5 +1,3 @@
-import createError from 'http-errors'
-
 function passDataToView(req, res, next) {
   res.locals.user = req.user ? req.user : null
   res.locals.googleClientID = process.env.GOOGLE_CLIENT_ID
@@ -8,8 +6,7 @@ function passDataToView(req, res, next) {
 
 function isLoggedIn(req, res, next) {
   if (req.isAuthenticated()) return next()
-  const notLoggedInError = createError(403, 'Please log in to view this page.')
-  return next(notLoggedInError)
+  res.redirect('/auth/google')
 }
 
 export {
