63b1a55839eec6fc2250648ae0347db1e75b2ca9
Annie Kuxa <kuxa.business@gmail.com>
2023-05-01 22:11:10 -0300

    fix indentation issues

--- controllers/games.js
+++ controllers/games.js
@@ -2,28 +2,28 @@ import { Game } from '../models/game.js'
 
 function index(req, res) {
   Game.find({})
-  .then(games => {
-    res.render('games/index', {
-      games,
-      title: '🕹️'
+    .then(games => {
+      res.render('games/index', {
+        games,
+        title: '🕹️'
+      })
     })
-  })
-  .catch(err => {
-    console.log(err)
-    res.redirect('/')
-  })
+    .catch(err => {
+      console.log(err)
+      res.redirect('/')
+    })
 }
 
 function create(req, res) {
   req.body.owner = req.user.profile._id
   Game.create(req.body)
-  .then(game => {
-    res.redirect('/games')
-  })
-  .catch(err => {
-    console.log(err)
-    res.redirect('/games')
-  })
+    .then(game => {
+      res.redirect('/games')
+    })
+    .catch(err => {
+      console.log(err)
+      res.redirect('/games')
+    })
 }
 
 function show(req, res) {
@@ -89,20 +89,20 @@ function update(req, res) {
 
 function deleteGame(req, res) {
   Game.findById(req.params.gameId)
-  .then(game => {
-    if (game.owner.equals(req.user.profile._id)) {
-      return Game.findByIdAndDelete(req.params.gameId)
-    }
-  })
-  .then(game => {
-    if (game) {
-      res.redirect('/games')
-    }
-  })
-  .catch(err => {
-    console.log(err)
-    res.redirect('/')
-  })
+    .then(game => {
+      if (game.owner.equals(req.user.profile._id)) {
+        return Game.findByIdAndDelete(req.params.gameId)
+      }
+    })
+    .then(game => {
+      if (game) {
+        res.redirect('/games')
+      }
+    })
+    .catch(err => {
+      console.log(err)
+      res.redirect('/')
+    })
 }
 
 
